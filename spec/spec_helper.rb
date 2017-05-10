a35e2ffe54458885a5edafb976b1eed8635ceb39
C80609A <c080609a@gmail.com>
2017-05-10 14:42:47 +0300

    develop: подключил `rspec-rails` и другие development_dependency гемы.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,33 @@
-require "bundler/setup"
-require "c80_contest"
+
+ENV['RAILS_ENV'] ||= 'test'
+
+require 'bundler/setup'
+require 'shoulda/matchers'
+require File.expand_path('../dummy/config/environment.rb', __FILE__)
+require 'rspec/rails'
+require 'c80_contest'
+
+Rails.backtrace_cleaner.remove_silencers!
+
+# Load support files
+# noinspection RubyResolve
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
-  config.example_status_persistence_file_path = ".rspec_status"
+  config.example_status_persistence_file_path = '.rspec_status'
 
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
 
   config.expect_with :rspec do |c|
     c.syntax = :expect
+  end
+end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
   end
 end
