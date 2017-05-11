4973c37861390205a34aa17ddf3fcb7d69f25497
C80609A <c080609a@gmail.com>
2017-05-11 10:32:43 +0300

    develop: в 'routes.rb' добавил формат 'js' по умолчанию к маршруту 'give_me_bid_form'

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 C80Contest::Engine.routes.draw do
   # match '/message_order', :to => 'mess#handle_message_order', :via => :post
-  match '/give_me_bid_form', :to => 'site#give_me_form', :via => :post
+  match '/give_me_bid_form', :to => 'site#give_me_form', :via => :post, :defaults => { :format => 'js' }
   # get 'index', :to => 'site#index'
 end
