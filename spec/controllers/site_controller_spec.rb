526ca27358e8ccbbf6817c6bf8f02d6d51c5f048
C80609A <c080609a@gmail.com>
2017-05-11 10:33:24 +0300

    develop: подпилил тест для site_controller

--- spec/controllers/site_controller_spec.rb
+++ spec/controllers/site_controller_spec.rb
@@ -6,10 +6,12 @@ RSpec.describe C80Contest::SiteController, :type => :controller do
   context do
     it do
       # get '/c80/index'
-      post :give_me_form
+      post :give_me_form, { :format => :js }
+      # byebug
       # post '/give_me_bid_form'
       # post :give_me_bid_form
       expect(response.status).to eq(200)
+      expect(response).to render_template('give_me_form')
     end
   end
 end
