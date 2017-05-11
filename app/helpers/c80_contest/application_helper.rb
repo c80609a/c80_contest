dbdbd98be05db7361a52b082046b8e5ba4ddf9a3
C80609A <c080609a@gmail.com>
2017-05-11 20:46:47 +0300

    develop: application_helper.rb: добавил методы render_bid_form и render_ok_message.

--- app/helpers/c80_contest/application_helper.rb
+++ app/helpers/c80_contest/application_helper.rb
@@ -1,4 +1,26 @@
 module C80Contest
   module ApplicationHelper
+
+    def render_bid_form(bid)
+
+      settings = Setting.first
+
+      render :partial => 'c80_contest/shared/bid_form',
+             :locals => {
+                 bid: bid,
+                 settings: settings
+             }
+
+    end
+
+    def render_ok_message
+      settings = Setting.first
+      render :partial => 'c80_contest/shared/ok_message',
+             :locals => {
+                 ok_text: settings.ok_text,
+                 ok_text_title: settings.ok_text_title
+             }
+    end
+
   end
 end
