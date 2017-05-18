827de9c551bc1dfb1b5b99080a48f93ad6a88129
C80609A <c080609a@gmail.com>
2017-05-18 19:05:49 +0300

    develop: если gem выключен, то все действия вернут 'none': - [X] обрабатываю `is_active` настройку в `ApplicationController` с помощью `before_filter`: возвращаю `:plain => 'none'` - [X] есть тест, который проверяет ответ (при выключенном gem-е) от 'SiteController#give_me_form'.

--- app/controllers/c80_contest/application_controller.rb
+++ app/controllers/c80_contest/application_controller.rb
@@ -1,5 +1,18 @@
 module C80Contest
   class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
+
+    # noinspection RubyResolve
+    before_filter :check_is_active
+
+    private
+
+    def check_is_active
+      # noinspection RubyResolve
+      unless Setting.first.is_active
+        render :plain => 'none'
+      end
+    end
+
   end
 end
--- spec/controllers/site_controller_spec.rb
+++ spec/controllers/site_controller_spec.rb
@@ -14,4 +14,15 @@ RSpec.describe C80Contest::SiteController, :type => :controller do
       expect(response).to render_template('give_me_form')
     end
   end
+
+  context 'Если библиотека выключена (Setting.is_active = false)' do
+    it 'контроллер должен вернуть plain text "none"' do
+      C80Contest::Setting.first.update_column(:is_active, false)
+      post :give_me_form, { :format => :js }
+      expect(response.status).to eq(200)
+      # byebug
+      expect(response.body).to eq 'none'
+    end
+  end
+
 end
