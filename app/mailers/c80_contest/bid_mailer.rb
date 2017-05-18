2ae38890a90b3b96ba3639b5dca06cef8ca9653d
C80609A <c080609a@gmail.com>
2017-05-18 16:23:37 +0300

    develop: BidMailer incoming (+tests).

---
+++ app/mailers/c80_contest/bid_mailer.rb
@@ -0,0 +1,25 @@
+module C80Contest
+  class BidMailer < ActionMailer::Base
+
+    include C80Contest::MailerHelper
+
+    default from: Proc.new { Setting.first.mail_from },
+            to: Proc.new { Setting.first.mail_to }
+
+    def send_letter(bid)
+
+      subj = Setting.first.message_letter_subj
+
+      Rails.logger.debug "<BidMailer.send_letter> subj: #{subj}, #{bid.as_json}"
+      @message = format_message(bid)
+
+      puts '<BidMailer.send_letter> Отправляем сообщение.'
+      mail(subject: subj) do |format|
+        format.html { render inline: '<%= @message.html_safe %>'}
+      end
+
+      #puts "#{SiteProps.first.mail_to}"
+    end
+
+  end
+end
---
+++ spec/mailers/previews/c80_contest/bid_mailer_preview.rb
@@ -0,0 +1,6 @@
+module C80Contest
+  # Preview all emails at http://localhost:3000/rails/mailers/c80_contest/bid_mailer
+  class C80Contest::BidMailerPreview < ActionMailer::Preview
+
+  end
+end
